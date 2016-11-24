d39f89541aa93043301cb8fa08bbc93b3b36c11d
Miguel Castillo Lloret <Miguel Castillo Lloret>
2016-11-24 23:27:56 +0100

    Improved search system, search GPU and help commands added

--- bot.py
+++ bot.py
@@ -14,14 +14,28 @@ def listener(mensajes):
 		if m.content_type == 'text':
 			print ("[" + str(chat_id) + "]:" + m.text)
 
+@mi_bot.message_handler(commands=['help'])
+def command_reviews(m):
+	chat_id = m.chat.id
+	mi_bot.send_message(chat_id, "Hi!, I am HardwareBot, nice to meet you! If you want general info of a Graphics Card, ask me with /GPUInfo + card name. For CPUs, say /CPUInfo + CPU Name. If you want a GPU review list, ask /reviewsGPUNVIDIA + GPU Name or /reviewsGPUAMD + GPU Name. Hope I can help you!")
+
 @mi_bot.message_handler(commands=['reviewsGPUAMD'])
 def command_reviews(m):
 	chat_id = m.chat.id
 	message = m.text[15:]
 	response = "Reviews of " + message + ":\n"
-	message = message.replace(" ", "-")
-	print ("https://www.techpowerup.com/reviewdb/Graphics-Cards/AMD/" + message)
-	reviewsweb = BeautifulSoup(urlopen("https://www.techpowerup.com/reviewdb/Graphics-Cards/AMD/" + message))
+	reviewsweb = BeautifulSoup(urlopen("https://www.techpowerup.com/reviewdb/Graphics-Cards/AMD/"))
+	tags = reviewsweb.find_all("a", class_="categories")
+	print(type(tags))
+	expresion = re.compile("\D*?"+message)
+	for aTag in tags:
+		print(aTag.string.strip())
+		print(expresion.match(aTag.string.strip()))
+		if aTag.string is not None and expresion.match(aTag.string.strip()):
+			urlTag = aTag['href']
+			print("https://www.techpowerup.com" + urlTag)
+			break
+	reviewsweb = BeautifulSoup(urlopen("https://www.techpowerup.com" + urlTag))
 	tags = reviewsweb.find_all("td", class_="name")
 	print(tags)
 	for aTag in tags:
@@ -38,9 +52,18 @@ def command_reviews(m):
 	chat_id = m.chat.id
 	message = m.text[18:]
 	response = "Reviews of " + message + ":\n"
-	message = message.replace(" ", "-")
-	print ("https://www.techpowerup.com/reviewdb/Graphics-Cards/AMD/" + message)
-	reviewsweb = BeautifulSoup(urlopen("https://www.techpowerup.com/reviewdb/Graphics-Cards/NVIDIA/" + message))
+	reviewsweb = BeautifulSoup(urlopen("https://www.techpowerup.com/reviewdb/Graphics-Cards/NVIDIA/"))
+	tags = reviewsweb.find_all("a", class_="categories")
+	print(type(tags))
+	expresion = re.compile("\D*?"+message)
+	for aTag in tags:
+		print(aTag.string.strip())
+		print(expresion.match(aTag.string.strip()))
+		if aTag.string is not None and expresion.match(aTag.string.strip()):
+			urlTag = aTag['href']
+			print("https://www.techpowerup.com" + urlTag)
+			break
+	reviewsweb = BeautifulSoup(urlopen("https://www.techpowerup.com" + urlTag))
 	tags = reviewsweb.find_all("td", class_="name")
 	print(tags)
 	for aTag in tags:
@@ -60,10 +83,14 @@ def command_reviews(m):
 	reviewsweb = BeautifulSoup(urlopen("https://www.techpowerup.com/cpudb/?mfgr%5B%5D=amd&mfgr%5B%5D=intel&class%5B%5D=desktop&class%5B%5D=server&released%5B%5D=y14_c&released%5B%5D=y11_14&released%5B%5D=y08_11&released%5B%5D=y05_08&released%5B%5D=y00_05&logo=&nCores=&process=&socket=&codename=&multi=&sort=name&q="))
 	tags = reviewsweb.find_all("a")
 	print(type(tags))
+	expresion = re.compile(".*?"+message)
 	for aTag in tags:
-		if aTag.string == message:
+		print(aTag.string)
+		if aTag.string is not None and expresion.match(aTag.string):
+			print(aTag.string)
 			urlTag = aTag['href']
 			print("https://www.techpowerup.com" + urlTag)
+			break
 	reviewsweb = BeautifulSoup(urlopen("https://www.techpowerup.com" + urlTag))
 	img = reviewsweb.find("img", class_="cpulogo")
 	urlretrieve("http:" + img['src'],"a.gif")
@@ -88,6 +115,46 @@ def command_reviews(m):
 				tdTag = tcTag.contents[3]
 				print(tdTag)
 				response = response + tdTag.string + "\n"
+	mi_bot.send_message(chat_id, response)
+
+@mi_bot.message_handler(commands=['GPUInfo'])
+def command_reviews(m):
+	chat_id = m.chat.id
+	message = m.text[9:]
+	response = "Info about " + message + ":\n"
+	reviewsweb = BeautifulSoup(urlopen("https://www.techpowerup.com/gpudb/?mfgr%5B%5D=amd&mfgr%5B%5D=nvidia&mobile=0&released%5B%5D=y14_c&released%5B%5D=y11_14&released%5B%5D=y08_11&released%5B%5D=y05_08&released%5B%5D=y00_05&generation=&chipname=&interface=&ushaders=&tmus=&rops=&memsize=&memtype=&buswidth=&slots=&powerplugs=&sort=released&q="))
+	tags = reviewsweb.find_all("a")
+	print(type(tags))
+	expresion = re.compile("\D*?"+message)
+	for aTag in tags:
+		if aTag.string is not None and expresion.match(aTag.string):
+			print(aTag.string)
+			urlTag = aTag['href']
+			print("https://www.techpowerup.com" + urlTag)
+			break
+	reviewsweb = BeautifulSoup(urlopen("https://www.techpowerup.com" + urlTag))
+	img = reviewsweb.find("img", title="Card Photo")
+	if img is not None:
+		urlretrieve("https://www.techpowerup.com" + img['src'],"a.jpg")
+		mi_bot.send_photo(chat_id, open('a.jpg','rb'))
+	mi_bot.send_message(chat_id, response)
+	response = ""
+	trTags = reviewsweb.find_all("tr")
+	print(trTags)
+	for tcTag in trTags:
+		if len(tcTag) >= 4:
+			if tcTag.contents[1].string is not None and tcTag.contents[3] is not None and tcTag.contents[3].string is not None :
+				thTag = tcTag.contents[1]
+				print(thTag)
+				tdTag = tcTag.contents[3]
+				print(tdTag)
+				response = response + thTag.string + " " +tdTag.string + "\n"
+			elif tcTag.contents[1].string is not None:
+				thTag = tcTag.contents[1]
+				print(thTag)
+				response = response + thTag.string + " unknown" "\n"
+			elif tcTag.contents[3] is not None and tcTag.contents[3].string is not None:
+				tdTag = tcTag.contents[3]
 	mi_bot.send_message(chat_id, response)
 
 mi_bot.set_update_listener(listener)
